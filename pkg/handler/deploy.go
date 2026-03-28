25c97256b7990ad70552cf340e0beda3283f937f
tonygcs <antonio.gallardo@algorath.com>
2026-03-28 15:14:38 +0100

    Use go template for containers definitions

--- pkg/handler/deploy.go
+++ pkg/handler/deploy.go
@@ -1,10 +1,12 @@
 package handler
 
 import (
+	"bytes"
 	"encoding/json"
 	"log"
 	"net/http"
 	"strings"
+	"text/template"
 
 	"agentsmanager/pkg/config"
 
@@ -20,8 +22,20 @@ type deployResponse struct {
 	ContainerID string `json:"containerId"`
 }
 
-func applyTemplate(s, name string) string {
-	return strings.ReplaceAll(s, "{{name}}", name)
+var templateFuncs = template.FuncMap{
+	"split": strings.Split,
+}
+
+func applyTemplate(s, name string) (string, error) {
+	tmpl, err := template.New("").Funcs(templateFuncs).Parse(s)
+	if err != nil {
+		return "", err
+	}
+	var buf bytes.Buffer
+	if err := tmpl.Execute(&buf, map[string]string{"name": name}); err != nil {
+		return "", err
+	}
+	return buf.String(), nil
 }
 
 type DeployHandler struct {
@@ -51,12 +65,30 @@ func (h *DeployHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	cmd := make([]string, len(workerCfg.Cmd))
 	for i, part := range workerCfg.Cmd {
-		cmd[i] = applyTemplate(part, name)
+		var err error
+		cmd[i], err = applyTemplate(part, name)
+		if err != nil {
+			log.Printf("template error in cmd for worker %s: %v", req.WorkerName, err)
+			http.Error(w, "deploy failed", http.StatusInternalServerError)
+			return
+		}
 	}
 
 	labels := make(map[string]string, len(workerCfg.Labels))
 	for k, v := range workerCfg.Labels {
-		labels[applyTemplate(k, name)] = applyTemplate(v, name)
+		key, err := applyTemplate(k, name)
+		if err != nil {
+			log.Printf("template error in label key for worker %s: %v", req.WorkerName, err)
+			http.Error(w, "deploy failed", http.StatusInternalServerError)
+			return
+		}
+		val, err := applyTemplate(v, name)
+		if err != nil {
+			log.Printf("template error in label value for worker %s: %v", req.WorkerName, err)
+			http.Error(w, "deploy failed", http.StatusInternalServerError)
+			return
+		}
+		labels[key] = val
 	}
 
 	env := make([]string, 0, len(workerCfg.Secrets))
